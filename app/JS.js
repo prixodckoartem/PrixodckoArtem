d13a5afec9afdcd42790aaa11981815d617f1756
SynthEave <prixodckoartem@gmail.com>
2025-09-10 21:29:26 +0300

    Add files via upload

---
+++ app/JS.js
@@ -0,0 +1,5 @@
+const num = 1;
+console.log("Переменная: " + num);
+var gif = 5;
+gif  = 90;
+console.log("Переменная: " + gif);
---
+++ app/script.js
@@ -0,0 +1,3 @@
+//console.log(31, 18, 79) // 1.1
+const numbers = [47, 52, 150]; 
+console.log(numbers.join(' ')); // 1.2
