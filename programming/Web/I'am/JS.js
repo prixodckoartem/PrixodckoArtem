11f2cfd2e33bca47e1822f4d7c21548c0fff8e53
SynthEave <prixodckoartem@gmail.com>
2025-09-26 17:55:50 +0300

    Add files via upload

---
+++ programming/Web/I'am/JS.js
@@ -0,0 +1,34 @@
+/*const num = 1;
+console.log("Переменная: " + num);
+var gif = 5;
+gif  = 90;
+console.log("Переменная: " + gif);
+*/
+/* = 15;
+
+if(num == '15') 
+    {
+    console.log("ok");{
+
+}
+    console.log('oK!')
+} else {
+    console.log('error')
+}
+const name = prompt('Введите своё имя: ');
+alert(name + 'Прив' + '!');
+
+let a = 10;
+let b = 100;
+let summ = a + b;
+
+console.log(summ)
+
+
+var nmm = prompt("Введите что то:")
+
+alert("Вы что то ввели и это: " + nmm + " вот только зачем это тебе?")
+*/
+
+
+
