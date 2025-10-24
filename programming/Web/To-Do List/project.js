e1e30e479022acc97b2bf7eb5bd76480fc0d3a2c
SynthEave <prixodckoartem@gmail.com>
2025-10-24 22:49:34 +0300

    Add files via upload

---
+++ programming/Web/To-Do List/project.js
@@ -0,0 +1,12 @@
+class TodoList {
+    constructor() {
+        this.tasks = [];
+        this.currentFilter = 'all';
+        this.init();
+    }
+}
+
+init() {
+    this.loadTasks();
+    this.
+}
