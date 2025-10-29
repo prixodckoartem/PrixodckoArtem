81fba66bc819ac980357538be0ae05667c14d3a9
SynthEave <prixodckoartem@gmail.com>
2025-10-29 21:50:27 +0300

    Add files via upload

--- programming/Web/I'am/Вычисления по формулам1.js
+++ programming/Web/I'am/Вычисления по формулам1.js
@@ -11,7 +11,7 @@ alert("При x = " + x + ", значение функции y = " + y);
 // Также выводим результат в консоль (для просмотра через F12)
 console.log("При x = " + x + ", значение функции y = " + y);    //2.1 a)
 */
-
+/*
 let a = parseFloat(prompt("Введите значение a:"));
 
 let y = 3 * Math.pow(a, 2) + 5 * a - 21;
@@ -19,6 +19,40 @@ let y = 3 * Math.pow(a, 2) + 5 * a - 21;
 alert("При a = " + a + ", значение функции y = " + y);
 
 console.log("При a = " + a + ", значение функции y = " + y);    //2.1 b)
+*/
 
 
+// function calculateFunction(a) {
+//     return Math.pow(a, 2) + 10; // 2.2
+// }
+
+
+// function functionA(a) {
+//     return 2 * Math.pow(a, 2) + 3 * a - 1; // 
+// }
+
+// function functionB(x) {
+//     return Math.sin(x) + Math.cos(x); // 2.3
+// }
+
+// function squarePerimeter(side) {
+//     return 4 * side;                  // 2.4
+// }
+
+// function circleDiameter(radius) {
+//     return 2 * radius;
+// }                                    //2.5
+
+// function horizonDistance(height) {
+//     const R = 6350; 
+//     return Math.sqrt(Math.pow(R + height, 2) - Math.pow(R, 2));
+// }
+
+// console.log(horizonDistance(0.1));       //2.6
+
+// function cubeProperties(edge) {
+//     const volume = Math.pow(edge, 3);
+//     const surfaceArea = 6 * Math.pow(edge, 2);
+//     return { volume, surfaceArea };
+// }                                               //2.7
 
